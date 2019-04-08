e97717b0929609fac448f4c4486893131f084d82
xuelang <LZJunity@radiantsoft.net>
2019-04-08 10:53:37 +0800

    Cosmos的Demo项目第一次上传

---
+++ Cosmos/Assets/Script/BgMove.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class BgMove : MonoBehaviour
+{
+    /// <summary>
+    /// 鼠标位置
+    /// </summary>
+    private Vector3 vec3;
+    /// <summary>
+    /// 物体位置
+    /// </summary>
+    private Vector3 pos;
+    /// <summary>
+    /// 背景位置
+    /// </summary>
+    public GameObject bgPos;
+    /// <summary>
+    /// 移动距离
+    /// </summary>
+    public float off;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    /// <summary>
+    /// 鼠标抬起
+    /// </summary>
+    public void MoveObject()
+    {
+        Vector3 off = Input.mousePosition - vec3;
+        vec3 = Input.mousePosition;
+        pos = pos + off;
+        transform.GetComponent<RectTransform>().position = pos;
+        //鼠标按下开启协程
+        StartCoroutine(Bgmove());
+    }
+    /// <summary>
+    /// 鼠标按下
+    /// </summary>
+    public void PointerDown()
+    {
+        vec3 = Input.mousePosition;
+        pos = transform.GetComponent<RectTransform>().position;
+        //鼠标抬起关闭协程
+        StopCoroutine(Bgmove());
+    }
+    /// <summary>
+    /// 协程控制背景移动
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator Bgmove()
+    {
+        //滚动背景
+        float move = 10.0f;//移动速度
+        Vector3 pos = bgPos.transform.localPosition;
+        pos.x -= move;
+        //控制移动范围（超过画布归零）
+        if (pos.x < -1280)
+        {
+            pos.x = 0;
+        }
+        bgPos.transform.localPosition = pos;
+        yield return new WaitForSeconds(0.1f);
+    }
+}
---
+++ Cosmos/Assets/Script/HSlider.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HSlider : MonoBehaviour
+{
+    // Start is called before the first frame update
+    /// <summary>
+    /// 定义横进度条
+    /// </summary>
+    public Slider bar;
+
+    public float duration = 1f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (bar.value < 1)
+        {
+            float value = 1 / duration * Time.deltaTime;
+            bar.value += value;
+        }
+    }
+
+    /// <summary>
+    /// 头像技能释放按钮
+    /// </summary>
+    public void HeadButton()
+    {
+            bar.value = 0;  
+        Debug.Log("我点击了按钮……");
+    }
+}
